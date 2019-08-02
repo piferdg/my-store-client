fcc0ae5ec16385b2254c414188b9ba83b17287a1
Danny Pifer <piferdg@gmail.com>
2019-08-01 21:01:32 -0600

    Added stripe on index.js and index.html. purchases are working in test

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -22,6 +22,30 @@ class ProductDetail extends Component {
       })
   }
 
+  initiateStripeCheckout = async () => {
+    console.log('initiating')
+    const stripe = window.Stripe('pk_test_CjBcpo4bXvJnpzPLBnrsIdlc00yvdzU5ly')
+
+    try {
+      // Initiate checkout session to get session id
+      const response = await fetch('http://localhost:4000/api/checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      const data = await response.json()
+      const sessionId = data.session.id
+  
+      // Redirect to checkout
+      const result = await stripe.redirectToCheckout({ sessionId })
+  
+    } catch (error) {
+      console.log('STRIPE ERROR', error)
+    }
+  }
+
   render () {
     return (
       <div>
@@ -31,6 +55,7 @@ class ProductDetail extends Component {
           <img src={this.state.product.img_url} alt='product' />
           <h3>{this.state.product.description}</h3>
           <h3>Price: ${this.state.product.price / 100}</h3>
+          <button onClick={this.initiateStripeCheckout}>Purchase</button>
       </Card>  
     </div>
     )
